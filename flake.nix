0ca7210e81896298efd3359f29a941a9ed373d73
Benji Vesterby <benji@codepros.org>
2025-09-20 17:10:04 -0400

    fix: updating several files

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-utils.url = "github:numtide/flake-utils";
     # Shared development environment (provides pinned Go + Zig 0.15.1 + common tools)
     dev-env.url = "github:spyderorg/dev-env";
-    zcap.url = "github:spyderorg/zcap";
     gomod2nix.url = "github:nix-community/gomod2nix";
   };
 
@@ -16,7 +15,6 @@
       nixpkgs,
       dev-env,
       flake-utils,
-      zcap,
       gomod2nix,
       ...
     }:
@@ -25,9 +23,6 @@
         nixpkgs: system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          zcapPkg = zcap.packages.${system}.zcap;
-          zcapLib = zcap.packages.${system}.zcap-static;
-
           # Pull toolchains / common tooling from centralized dev-env flake
           inherit (dev-env.packages.${system}) zigPackages goPackages commonPackages;
           buildGoApp = gomod2nix.legacyPackages.${system}.buildGoApplication;
@@ -104,11 +99,6 @@
                   export GH_TOKEN="$TOK"
                 fi
               fi
-              # Provide zcap prefixes for Makefile and GoReleaser
-              export ZCAP_PREFIX=${zcapPkg}
-              export ZCAP_STATIC_PREFIX=${zcapLib}
-              # Provide Zig lib dir for static compiler runtime when needed
-              export ZIG_LIB_DIR=${pkgs.zig}/lib/zig
             '';
             env = {
               GOPRIVATE = goPrivate;
@@ -120,27 +110,19 @@
               # Base developer toolchain (Go + Zig + shared utilities)
               [
                 goPackages
-                zigPackages
                 commonPackages
               ]
-              ++ [
-                zcapPkg
-                zcapLib
-                pkgs.gh
-                pkgs.tshark
-                pkgs.suricata
-                pkgs.tcpdump
-              ];
+              ++ [ ];
           };
 
           packages = {
-            pcapinfo = buildGoApp (
+            canary = buildGoApp (
               {
-                pname = "pcapinfo";
+                pname = "canary";
                 version = "dev";
                 src = self;
                 modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapinfo" ];
+                subPackages = [ "cmd/canary" ];
                 GOPRIVATE = goPrivate;
                 GONOPROXY = goPrivate;
                 GONOSUMDB = goPrivate;
@@ -148,139 +130,13 @@
                   pkgs.gh
                   pkgs.git
                 ];
-                preBuild = preBuildWithGh;
-              }
-              // gitAuthEnv
-            );
-
-            pcapnginfo = buildGoApp (
-              {
-                pname = "pcapnginfo";
-                version = "dev";
-                src = self;
-                modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapnginfo" ];
-                GOPRIVATE = goPrivate;
-                GONOPROXY = goPrivate;
-                GONOSUMDB = goPrivate;
-                nativeBuildInputs = [
-                  pkgs.gh
-                  pkgs.git
-                ];
-                preBuild = preBuildWithGh;
-              }
-              // gitAuthEnv
-            );
-
-            pcapconv = buildGoApp (
-              {
-                pname = "pcapconv";
-                version = "dev";
-                src = self;
-                modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapconv" ];
-                GOPRIVATE = goPrivate;
-                GONOPROXY = goPrivate;
-                GONOSUMDB = goPrivate;
-                nativeBuildInputs = [
-                  pkgs.gh
-                  pkgs.git
-                ];
-                preBuild = preBuildWithGh;
-              }
-              // gitAuthEnv
-            );
-
-            pcapfilter = buildGoApp (
-              {
-                pname = "pcapfilter";
-                version = "dev";
-                src = self;
-                modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapfilter" ];
-                GOPRIVATE = goPrivate;
-                GONOPROXY = goPrivate;
-                GONOSUMDB = goPrivate;
-                nativeBuildInputs = [
-                  pkgs.gh
-                  pkgs.git
-                ];
-                preBuild = preBuildWithGh;
-              }
-              // gitAuthEnv
-            );
-
-            pcapdump = buildGoApp (
-              {
-                pname = "pcapdump";
-                version = "dev";
-                src = self;
-                modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapdump" ];
-                CGO_ENABLED = 1;
-                tags = [ "zcap" ];
-                buildInputs = [
-                  zcapPkg
-                  zcapLib
-                ];
-                nativeBuildInputs = [
-                  pkgs.pkg-config
-                  zigPackages
-                  pkgs.gh
-                  pkgs.git
-                ];
-                CC = "${zigPackages}/bin/zig cc";
-                CXX = "${zigPackages}/bin/zig c++";
-                CGO_CFLAGS = "-I${zcapLib}/include -I${zcapPkg}/include";
-                CGO_LDFLAGS = "-Wl,-Bstatic -L${zcapLib}/lib -lzcap -Wl,-Bdynamic -Wl,-rpath,${zcapPkg}/lib";
-                GOPRIVATE = goPrivate;
-                GONOPROXY = goPrivate;
-                GONOSUMDB = goPrivate;
-                preBuild = preBuildWithGh;
-              }
-              // gitAuthEnv
-            );
-
-            # Fully static MUSL build using zig (Linux x86_64 only)
-            pcapdump-static = buildGoApp (
-              {
-                pname = "pcapdump-static";
-                version = "dev";
-                src = self;
-                modules = ./gomod2nix.toml;
-                subPackages = [ "cmd/pcapdump" ];
-                CGO_ENABLED = 1;
-                tags = [
-                  "zcap"
-                  "netgo"
-                  "osusergo"
-                ];
-                buildInputs = [ zcapLib ];
-                nativeBuildInputs = [
-                  zigPackages
-                  pkgs.gh
-                  pkgs.git
-                ];
-                # Restrict to Linux amd64/aarch64 via target flags; zig will static link musl
-                CC = "${zigPackages}/bin/zig cc -target x86_64-linux-musl";
-                CXX = "${zigPackages}/bin/zig c++ -target x86_64-linux-musl";
-                CGO_CFLAGS = "-I${zcapLib}/include";
-                CGO_LDFLAGS = "-static -L${zcapLib}/lib -lzcap -L${zigPackages}/lib/zig -l:libcompiler_rt.a";
-                ldflags = [
-                  "-s"
-                  "-w"
-                  "-linkmode=external"
-                ];
-                GOPRIVATE = goPrivate;
-                GONOPROXY = goPrivate;
-                GONOSUMDB = goPrivate;
                 preBuild = preBuildWithGh;
               }
               // gitAuthEnv
             );
           };
 
-          defaultPackage = packages.pcapdump;
+          defaultPackage = packages.canary;
         };
     in
     flake-utils.lib.eachDefaultSystem (system: flakeForSystem nixpkgs system);
